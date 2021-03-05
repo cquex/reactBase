5b36d4a080a2381208f3f3abf02e70b7cdbb77f8
cquexm <carlquex@gmail.com>
2021-03-05 12:46:58 -0600

    EsLint

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDom from "react-dom";
+import HellowWorld from "./components/HellowWorld";
+
+ReactDom.render(<HellowWorld></HellowWorld>, document.getElementById('app'));
